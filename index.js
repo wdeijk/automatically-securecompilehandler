834341f4a33aff271037e11a313d70832d37e3fa
wdeijk <gs1daniynha@hotmail.com>
2024-05-08 11:03:58 +0700

    feat

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-function findSubstring(s, words) {
-  if (s.length === 0 || words.length === 0) return [];
-  const wordMap = new Map();
-  for (const word of words) {
-    wordMap.set(word, (wordMap.get(word) || 0) + 1);
-  }
-  const wordLength = words[0].length;
-  const totalLength = words.length * wordLength;
-  const result = [];
-  for (let i = 0; i <= s.length - totalLength; i++) {
-    const substring = s.substring(i, i + totalLength);
-    if (isValid(substring, new Map(wordMap))) {
-      result.push(i);
-    }
-  }
-  return result;
-  function isValid(substring, wordMap) {
-    const wordCount = new Map();
-    for (let i = 0; i < substring.length; i += wordLength) {
-      const word = substring.substring(i, i + wordLength);
-      wordCount.set(word, (wordCount.get(word) || 0) + 1);
-    }
-    for (const [key, value] of wordMap) {
-      if ((wordCount.get(key) || 0) !== value) return false;
-    }
-    return true;
-  }
+function minDepth(root) {
+  if (!root) return 0;
+  if (!root.left && !root.right) return 1;
+  let min = Infinity;
+  if (root.left) min = Math.min(min, minDepth(root.left));
+  if (root.right) min = Math.min(min, minDepth(root.right));
+  return min + 1;
 }
