3a82a8c050a67ed5b6e3ec5356f62e289588c82e
wdeijk <gs1daniynha@hotmail.com>
2024-05-11 21:21:04 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-function coinChange(coins, amount) {
-  const dp = new Array(amount + 1).fill(amount + 1);
-  dp[0] = 0;
-  for (const coin of coins) {
-    for (let i = coin; i <= amount; i++) {
-      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-    }
-  }
-  return dp[amount] > amount ? -1 : dp[amount];
+function maxDepth(root) {
+  if (!root) return 0;
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 }
